b0be7c160a6f70dfc75420218f18bad7b41afec2
Sinnema1 <jmanning@email.arizona.edu>
2024-12-20 19:30:38 -0700

    pointing proxy to deployed URL

--- client/src/services/eventService.ts
+++ client/src/services/eventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Event } from '../interfaces/Event';
 
-const BASE_URL = 'http://localhost:3001/api/events';
+const BASE_URL = 'https://sound-and-scene-1.onrender.com';
 
 
 // Fetch events by zipcode from the server-side proxy.
